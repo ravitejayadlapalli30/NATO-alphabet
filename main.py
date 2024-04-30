3fa953533783122e7b3cd72a69f004c2b3d2272f
Ravi <ravitejayadlapalli@gmail.com>
2024-04-30 11:03:54 +0530

    Added exceptions.

--- main.py
+++ main.py
@@ -32,11 +32,16 @@ phonetic_dic = {row.letter: row.code for (index, row) in data.iterrows()}
 
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dic[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-word = input("Enter a word:").upper()
-
-output_list = [phonetic_dic[letter] for letter in word]
-
-print(output_list)
+generate_phonetic()
 
 
